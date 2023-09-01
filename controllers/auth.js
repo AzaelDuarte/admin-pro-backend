108cb3539930daf9ead1a91219ad174acb948750
Azael Duarte <azaelduarte999@gmail.com>
2023-08-31 23:20:13 -0600

    Ultimos cambios backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -89,13 +89,24 @@ const googleSingIn = async (req, res = response) => {
             msg: 'Token de google no es correcto'
         });
     }
+}
 
 
+const renewToken = async (req, res = response) => {
 
+    const uid = req.id;
+
+    const token = await generarJWT(uid);
+
+    res.json({
+        ok:true,
+        token
+    });
 }
 
 
 module.exports = {
     login,
-    googleSingIn
+    googleSingIn,
+    renewToken
 }
